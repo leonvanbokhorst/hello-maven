fd66ad0420b5e8ba165b1af2257539bad97971cd
Leon van Bokhorst <leonvanbokhorst@users.noreply.github.com>
2019-02-15 19:55:46 +0100

    Basic logger

--- src/main/java/com/leonvanbokhorst/HelloMaven.java
+++ src/main/java/com/leonvanbokhorst/HelloMaven.java
@@ -1,7 +1,14 @@
 package com.leonvanbokhorst;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+  private static final Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello Maven");
+    log.info("Hello info");
+    log.debug("Hello debug");
+    log.error("Boom!");
   }
 }
